fdb9a191224856364a73c4b3faf8e852ea3ffb1a
Daniel J <daniel.jonsson@consid.se>
2025-09-30 17:05:46 +0200

    test

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -3,6 +3,7 @@ import { useAuth } from '@/app/providers/authProvider';
 
 export function AuthButton() {
   const { user } = useAuth();
+
   return (
     <div className="flex items-center justify-end w-full gap-4 mt-2 mb-4 mr-2">
       <span className="hidden lg:block">

5ef27bc3ac7d28760fabc8058b92d642211ca905
Daniel J <daniel.jonsson@consid.se>
2025-10-16 14:41:28 +0200

    log out fix

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -7,8 +7,8 @@ import { useRouter } from 'next/navigation';
 export function LogoutButton() {
   const router = useRouter();
   const logout = async () => {
-    await supabase.auth.signOut();
     router.push('/auth/login');
+    await supabase.auth.signOut();
   };
 
   return (
